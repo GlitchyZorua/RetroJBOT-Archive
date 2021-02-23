f327c4b8534db051f843adf6131522e841c719cc
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2021-02-22 19:19:06 -0800

    Add files via upload

--- Commands/Fun/cock.js
+++ Commands/Fun/cock.js
@@ -1,6 +1,7 @@
 module.exports = {
-    name: 'cock',
+    name: 'ping',
     async execute(client, message, args) {
-        message.reply('COCK')
+        message.channel.send(":ping_pong: Pong!  `" + Math.floor(client.ws.ping) + ' ms`');
+    console.log("[MSG] :ping_pong: Pong!  `" + Math.floor(client.ws.ping) + ' ms`');
     }
 }
