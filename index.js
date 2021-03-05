5f62afe86516702aed28a2c3b541b67fb42ad3e4
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2021-03-05 11:55:45 -0800

    Replaced it with a newer version of it

--- index.js
+++ index.js
@@ -1,15 +1,72 @@
+var version = "13.0.0C RetroJBOT Canary Orange Edition Again...";
+const animals = require('random-animals-api');
+var ProgressBar = require('progress');
+var bar = new ProgressBar(':bar :current/:total', { total: 100 });
+console.log("########  ######## ######## ########   #######        ## ########   #######  ########")
+console.log("##     ## ##          ##    ##     ## ##     ##       ## ##     ## ##     ##    ## ")
+console.log("##     ## ##          ##    ##     ## ##     ##       ## ##     ## ##     ##    ## ")
+console.log("########  ######      ##    ########  ##     ##       ## ########  ##     ##    ## ")
+console.log("##   ##   ##          ##    ##   ##   ##     ## ##    ## ##     ## ##     ##    ##  ")
+console.log('##     ## ########    ##    ##     ##  #######   ######  ########   #######     ##    ')
+console.log("                                          Canary")
+console.log('                            ╭─────────────────────────────╮')
+console.log('                            │ Connecting to discord.js... │')
+console.log('                            ╰─────────────────────────────╯')
+console.log('                    v'+version)
 const Discord = require('discord.js')
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
+
+
+String.prototype.removepings = function() {
+    return this
+        .replace(/(@(?:everyone|here))/gi, 'no') //@everyone/here ping
+        .replace(/<@&(\d+)>/gi, "no") //role ping
+        .replace(/<@!?(\d+)>/gi, "no") //user ping
+        .replace("nigger", "no")
+        .replace("Nigger", "no")
+};
+String.prototype.rpremovepings = function() {
+    return this
+        .replace(/(@(?:everyone|here))/gi, 'no')
+        .replace(/<@&(\d+)>/gi, "no")
+        .replace("nigger", "no")
+        .replace("Nigger", "no")
+};
+String.prototype.aremovepings = function() {
+  return this
+      .replace('`', '\'')
+      .replace(/<@&(\d+)>/gi, "no")
+      .replace(/(@(?:everyone|here))/gi, 'no')
+      .replace(/<@&(\d+)>/gi, "no")
+      .replace("nigger", "no")
+      .replace("Nigger", "no")
+};
+String.prototype.aremovepingsh = function() {
+  return this
+      .replace('`', '\'')
+      .replace(/<@&(\d+)>/gi, "(a)")
+      .replace(/(@(?:everyone|here))/gi, '(a)')
+      .replace(/<@&(\d+)>/gi, "no")
+      .replace("nigger", "no")
+      .replace("Nigger", "no")
+};
+
+
 
 const client = new Discord.Client()
 require('dotenv').config()
 client.login(process.env.TOKEN)
 client.commands = new Discord.Collection()
 
+
 let errors = []
 
+console.log("╭───────────────────────────────────╮")
+console.log("│ Connected to discord. Now finding │")
+console.log("| javascript files.                 │")
+console.log("╰───────────────────────────────────╯")
 const modules = fs
     .readdirSync('Commands')
     .filter(file => fs.statSync(path.join('Commands', file)).isDirectory())
@@ -21,21 +78,22 @@ modules.forEach(module => {
         .readdirSync(path.resolve(`Commands/${module}`))
         .filter(file => !fs.statSync(path.resolve('Commands', module, file)).isDirectory())
         .filter(file => { return file.endsWith('.js')})
-    console.log(chalk.blueBright(`╭────────────────────┬──╮`))
+    // console.log(chalk.blueBright(`╭────────────────────┬──╮`))
     CMDFiles.forEach(CMD => {
         try {
             const command = require(`./Commands/${module}/${CMD}`)
             const FittedCMDName = `${command.name}`.padEnd(20)
-            console.log(chalk.blueBright(`│${FittedCMDName}│✅│\n├────────────────────┼──┤`))
+            //console.log(chalk.blueBright(`│${FittedCMDName}│✅│\n├────────────────────┼──┤`))
             command.module = module
             client.commands.set(command.name, command)
+            bar.tick()
         } catch (error) {
             const FittedCMDName = `${CMD}`.padEnd(20)
-            console.log(chalk.blueBright(`│${FittedCMDName}│❌│\n├────────────────────┼──┤`))
+            //console.log(chalk.blueBright(`│${FittedCMDName}│❌│\n├────────────────────┼──┤`))
             errors.push(error)
         }
     })
-    console.log(chalk.blueBright(`╰────────────────────┴──╯`))
+   // console.log(chalk.blueBright(`╰────────────────────┴──╯`))
 })
 if (errors.length > 0) {
     console.log(errors)
@@ -47,14 +105,15 @@ if (errors.length > 0) {
 fs.readdir('./Events', (err, files) => {
     if (err) return console.log(chalk.red(err))
     console.log(chalk.green("Events Handler"))
-    console.log(chalk.blueBright(`╭────────────────────┬──╮`))
+ //   console.log(chalk.blueBright(`╭────────────────────┬──╮`))
     files.forEach(file => {
         if (!file.endsWith('.js')) return
         const evt = require(`./Events/${file}`)
         let evtName = file.split('.')[0]
         const FittedEVTName = `${evtName}`.padEnd(20)
-        console.log(chalk.blueBright(`│${FittedEVTName}│✅│`))
+        //  console.log(chalk.blueBright(`│${FittedEVTName}│✅│`))
         client.on(evtName, evt.bind(null, client))
     })
-    console.log(chalk.blueBright(`╰────────────────────┴──╯`))
+   // console.log(chalk.blueBright(`╰────────────────────┴──╯`))
 })
+
