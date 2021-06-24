11e3c6388d43a2ae3dffb4fff7ac9f4ac1fbbd0b
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2021-06-24 10:54:18 -0700

    Add files via upload

--- Commands/General/ping.js
+++ Commands/General/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'ping',
     async execute(client, message, args) {
-        message.channel.send(":ping_pong: Pong!  `" + Math.floor(client.ws.ping) + ' ms`');
+        message.channel.send(":ping_pong: Pong!`" + Math.floor(client.ws.ping) + ' ms`');
     console.log("[MSG] :ping_pong: Pong!  `" + Math.floor(client.ws.ping) + ' ms`');
     }
 }
