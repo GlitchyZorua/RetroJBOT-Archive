64721bc543ed1b52a05b54f6c18ebb1b4d3bf183
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2021-07-11 13:39:27 -0700

    Add files via upload

--- Commands/Fun/coinflip.js
+++ Commands/Fun/coinflip.js
@@ -2,6 +2,6 @@ var coin = ['Heads', 'Tails']
 module.exports = {
     name: 'coinflip',
     async execute(client, message) {
-      message.channel.send('The coin landed on ' + "**" + coin[Math.floor(Math.random() * coin.length)] + "**")
+      message.channel.send(':coin: The coin landed on ' + "**" + coin[Math.floor(Math.random() * coin.length)] + "**!")
         }
 }
