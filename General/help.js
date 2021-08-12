cf493d36cad6aeda6f1320018192b60a99555d39
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2021-08-12 11:20:13 -0700

    Delete General directory

--- General/help.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'help',
-    async execute(client, message, args) {
-    message.channel.send(':scroll: Command List: <https://urls.ziad87.net/oepXj>\n:bug: Bug or suggestion? Report it in our discord server: j.invite, or suggest it in j.feedback\n\n\n**PLEASE READ**\n\nThe url shorter, https://urls.ziad87.net/, has been blocked by some anti-viruses. Please report it as false positive. Its fully encypted, and has no spooky stuff in it. Thank you for your corporation. If you need the unshortened link click here: <https://gist.github.com/Jacobwworkman/e2c1ce52e0c1edeab2405e8d3e7259b0>. To talk to ziad87 about this, please shoot him a dm at ziad87#3990 (not the creator of the bot)');
-}
-}
