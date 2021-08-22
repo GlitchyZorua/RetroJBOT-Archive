94ad822bf0f3b4113557ef8ecf08c29e97e57ad0
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2021-08-22 10:20:41 -0700

    Delete Utilities directory

--- Utilities/unixtime.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'unixtime',
-    async execute(client, message) {
-      const unixTime = Math.floor(Date.now() / 1000);
-      message.channel.send("🕒 " + unixTime)
-      return
-    }
-}
