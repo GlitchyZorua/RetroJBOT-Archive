2d85a45c76fb0f46ba22abc1a5ea1b782fa77e81
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2022-01-02 14:33:24 -0800

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -57,7 +57,7 @@ module.exports = (client, message) => {
         console.log(chalk.red(`(${message.author.id} || ${message.author.tag}) An error occurred: Command not found.`))
         message.channel.send(new Discord.MessageEmbed()
         .setColor('RED')
-        .setDescription('Invalid Command. If in doubt, check if: \n- You spelled the command correctly\n- Check that its a valid command (j.help)\n - If you want, you can suggest this command with `j.feedback <text>`')
+        .setDescription('Invalid Command. If in doubt, check if: \n- You spelled the command incorrectly\n- Check that its a valid command (j.help)\n - If you want, you can suggest this command with `j.feedback <text>`')
         .setTitle(':no_entry: Oops.')
         )
         }
