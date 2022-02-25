956ef83dbee8d9340b50d6f6559aa2b6d96a74c6
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2022-02-25 15:16:40 -0800

    yaaaayyyy

--- Events/message.js
+++ Events/message.js
@@ -1,16 +1,24 @@
 const chalk = require('chalk')
 const Discord = require('discord.js')
 const fs = require('fs-extra')
+const { isAnyArrayBuffer } = require('util/types')
 var botbans = fs.readJSONSync('./botbans.txt')
+var eee = fs.readJSONSync('./excludedservers.txt')
 var disableddms = 0
 var disableinteractions = 0
+var logmode = 0
 module.exports = (client, message) => {
+    if (logmode == 1){
+        //For testing purposes only. doesn't save messages.
+        console.log(`(${message.author.id}) ${message.author.tag}: ${message.content}`)
+        return
+    }
     if (message.author.bot) return
     if (message.channel.type === 'dm') {
     if (disableddms === 1){
         message.channel.send(":no_entry: DMS are disabled by the bot creator.");
         return
-         }
+        }
     }
         var prefix = process.env.Prefix
         var escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
@@ -20,7 +28,11 @@ module.exports = (client, message) => {
                 //console.log(matchedPrefix.length)
     
         console.log(chalk.yellow(`(${message.author.id} || ${message.author.tag}) Checking client to see if command`))
-
+        if (eee.es.includes(message.guild.id)){
+        console.log(chalk.red(`(${message.author.id} || ${message.author.tag}) An error occurred: Server Excluded.`))
+         message.channel.send('⛔ **Oops. This server is excluded from RetroJBOT, Sorry!**')
+         return
+        }
         if (botbans.bans.includes(message.author.id)) {
             console.log(chalk.red(`(${message.author.id} || ${message.author.tag}) An error occurred: User account id banned`))
             message.channel.send(":no_entry: This account has been blocked from RetroJBOT.\n\nTo appeal, please visit https://forms.gle/njmHVkYrFgCUckWbA. Any ALT accounts will be punished.")
